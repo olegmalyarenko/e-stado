8755829b006abffbf554de63a406ed3604c8bae9
olegmalyarenko <oi.malyarenko@gmail.com>
2020-07-10 16:09:51 +0300

    feat: second quiz range styles

--- src/app.js
+++ src/app.js
@@ -7,5 +7,6 @@ fistButton.addEventListener('click', function(e) {
    document.querySelector(".first-quiz").style.display = "none";
    document.querySelector(".second-quiz").style.display = "block";
    document.querySelector(".range").classList.add('half-green');
+   document.querySelector(".middle-dot").classList.add('green-dot');
    
 })
