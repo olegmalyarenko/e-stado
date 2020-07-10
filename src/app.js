ba3000f3de9cd5dc8ab61e0f81311e9fd6d974d0
olegmalyarenko <oi.malyarenko@gmail.com>
2020-07-10 15:47:38 +0300

    fix: style errors

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 import "./scss/main.scss";
+
+const fistButton = document.querySelector('.next-button');
+
+fistButton.addEventListener('click', function(e) {
+    console.log('dsdfd');
+   document.querySelector(".first-quiz").style.display = "none";
+   document.querySelector(".second-quiz").style.display = "block";
+   document.querySelector(".range").classList.add('half-green');
+   
+})
